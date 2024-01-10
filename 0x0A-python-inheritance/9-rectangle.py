ea7e9004651772a1a212e0a43797ca0219c393a2
elsaaeid <saidsadaoy@gmail.com>
2024-01-10 03:57:17 +0200

    deploy changes

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-""" New class """
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""Define class Rectangle, inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ New class Rectangle that inherist from Base Geometry"""
+    """Defines a Rectangle class instance."""
+
     def __init__(self, width, height):
+        """
+            Intialize a new Rectangle instance.
+
+            Args:
+                width (int): The width of the new Rectangle instance.
+                height (int): The height of the new Rectangle instance.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """ Print rectangle """
-        return (self.__height * self.__width)
+        """ Returns the area of the current Rectangle instance. """
+        return self.__width * self.__height
 
     def __str__(self):
-        """ Return str """
-        return("[{}] {}/{}".format(
-            self.__class__.__name__, self.__width, self.__height))
+        """ Returns the representation of the Rectangle instance."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
