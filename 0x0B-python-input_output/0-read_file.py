b7a27ccadea482349e029b68695321d987383262
elsaaeid <saidsadaoy@gmail.com>
2024-01-10 03:18:51 +0200

    deploy changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-""" Function that reads a text file (UTF8) and prints it to stdout """
+"""Module 0-read_file.py
+creates a function that reads a text file.
+"""
 
 
 def read_file(filename=""):
-    """ Reads UFT-8 File """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
-        
+    """a function that reads a text file(UTF8)
+    and prints it to stdout.
+
+    Args:
+       - filename: name of the file
+    """
+    with open(filename) as myfile:
+        read_text = myfile.read()
+        print(read_text, end="")
