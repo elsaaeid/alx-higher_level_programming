48c4733f223f13a8058006caa2d85c896b40270c
elsaaeid <saidsadaoy@gmail.com>
2024-03-12 21:31:58 +0200

    deploy changes

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,23 @@
 #!/usr/bin/node
-function add (a, b) {
-  return a + b;
-  }
 
-  console.log(add(Number(process.argv[2]), 
-  Number(process.argv[3])));
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both a and b must be numbers');
+  }
+  return a + b;
+}
+
+if (process.argv.length < 4) {
+  console.error('Usage: node add.js <number1> <number2>');
+  process.exit(1);
+}
+
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.error('Both arguments must be valid numbers');
+  process.exit(1);
+}
+
+console.log(add(arg1, arg2));
