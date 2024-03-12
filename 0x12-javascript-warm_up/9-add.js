bec10e26169e394933fcdcad878ec28e6a4ccb5e
elsaaeid <saidsadaoy@gmail.com>
2024-03-12 21:55:08 +0200

    deploy changes

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,5 +2,4 @@
 function add (a, b) {
     return a + b;
   }
-  const result = add(Number(process.argv[2]), Number(process.argv[3]));
-  console.log(`The sum is ${result}.`);
+  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
