4736d684447d2878cc3dff9afcded6629ec5b9d0
elsaaeid <saidsadaoy@gmail.com>
2024-03-18 03:55:18 +0200

    deploy changes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -39,4 +39,3 @@ if __name__ == "__main__":
     # Clean up process
     cur.close()
     conn.close()
-    
--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     # Create an engine that stores data in 'database.db'
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
