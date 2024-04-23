8d029944b4119b8ae13b6ac3e9f8a8e2b6ad88f9
elsaaeid <saidsadaoy@gmail.com>
2024-04-23 10:05:43 -0700

    deploy changes

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let movieId = process.argv[2];
+
+let url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, function (error, response, body) {
+	if (error) {
+		console.log(error);
+	} else {
+		let movie = JSON.parse(body);
+		let characters = movie.characters;
+
+		characters.forEach(characterUrl => {
+			request(characterUrl, function (error, response, body) {
+				if (error) {
+					console.log(error);
+				} else {
+					let character = JSON.parse(body);
+					console.log(character.name);
+				}
+			});
+		});
+	}
+});
