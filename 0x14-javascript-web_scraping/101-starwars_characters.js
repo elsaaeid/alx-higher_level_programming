99b6162ac191a63912b2e0125127b1fe0bd44b81
elsaaeid <saidsadaoy@gmail.com>
2024-04-26 03:00:12 -0700

    deploy changes

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,36 @@
-#!/usr/bin/node
-
 const request = require('request');
 
 const movieId = process.argv[2];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(url, error, response, body => {
+request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
   } else {
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-    characters.forEach(characterUrl => {
-      request(characterUrl, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
+    const characterPromises = characters.map(function (characterUrl) {
+      return new Promise(function (resolve, reject) {
+        request({ url: characterUrl, json: true }, function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(body.name);
+          }
+        });
       });
     });
+
+    Promise.all(characterPromises)
+      .then(function (characterNames) {
+        characterNames.forEach(function (characterName) {
+          console.log(characterName);
+        });
+      })
+      .catch(function (error) {
+        console.error('Error:', error);
+      });
   }
 });
