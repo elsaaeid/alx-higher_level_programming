f9509c2a69c0e1f71bd794064524b54e132533b8
elsaaeid <saidsadaoy@gmail.com>
2024-04-26 02:13:53 -0700

    deploy changes

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', function (err, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(body);
-}
+fs.readFile(filePath, 'utf8', function (error, content) {
+  console.log(error || content);
 });
